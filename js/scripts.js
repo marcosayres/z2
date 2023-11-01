e5a67cd752462d47ad3e7e519af5e20f32b0cf6c
marcosayres <marcosayres.brasil@gmail.com>
2023-11-01 12:46:54 -0300

    Z2 Inicial

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+// Defina o caminho completo para a imagem que você deseja usar como papel de parede
+var imagePath = "./assets/logo1.png";
+
+// Use o seguinte código para definir a imagem como papel de parede
+document.body.style.backgroundImage = "url('" + imagePath + "')";
+document.body.style.backgroundSize = "cover";
+document.body.style.backgroundRepeat = "no-repeat";
